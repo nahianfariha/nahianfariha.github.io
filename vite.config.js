30d2099fcb2d3042a11c71f4670e41d0a15c1192
Nahian Fariha <nahianfarihaofficial@gmail.com>
2025-11-01 01:28:46 +0600

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import flowbiteReact from "flowbite-react/plugin/vite";
 
 export default defineConfig({
-  plugins: [react(), flowbiteReact()],
-  base: '/nahianfariha.github.io/',
+  plugins: [react()],
+  base: './',
 })
