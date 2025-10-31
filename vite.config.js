442436407f8ca9a1024ed52bfe1daebc154f0ad5
Nahian Fariha <nahianfarihaofficial@gmail.com>
2025-10-31 23:10:33 +0600

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import flowbiteReact from "flowbite-react/plugin/vite";
 
 export default defineConfig({
   plugins: [react(), flowbiteReact()],
-  base: '/dotpicturesstudio',
+  base: '/nahianfariha.github.io/',
 })
